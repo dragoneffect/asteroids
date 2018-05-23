faeab422a0f882aeeaddd067449b190ea547498f
Unknown <deadgoldlion@gmail.com>
2018-05-23 13:03:50 +0700

    Added ability for red ship

--- include/global.h
+++ include/global.h
@@ -19,11 +19,13 @@ const long long int earthlings = 10000000000;
 const long long int min_survivors = 2000000000;
 //ширина кораблей/2
 int ship_blue_width = 14;
+int ship_red_width = 23;
 //для начального положения кораблей на экране
 int start_x_blue = windowWidth / 2;
 int start_x_red = (windowWidth / 2) - 50;
+int start_x_assist = (windowWidth / 2) - 100;
 int start_ship_y = windowHeight - 80;
-
+int start_y_assist = windowHeight - 60;
 int firstToDraw = 0;
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include "../include/global.h"
 #include "../include/menu.h"
-#include "../include/bullet.h"
 #include "../include/Model.h"
 #include "../include/Ship.h"
+#include "../include/assistant.h"
+#include "../include/bullet.h"
 
 
 bool is_it_the_end(Ship ship1, Ship ship2, long long int people) {
@@ -24,6 +25,7 @@ bool first_to_draw(Ship ship, Ship ship_2) {
     return true;
 }
 
+
 int main() {
   int count = 0;
   sf::Font font;
@@ -33,21 +35,49 @@ int main() {
   sf::Text text;
   text.setFont(font);
   Model map("background.png");
-  //изначальная позиция корабля
+  //изначальная позиция кораблей
   Ship ship(start_x_blue, start_ship_y, "pl1.png", Keyboard::Left,
             Keyboard::Right);
   Ship ship_2(start_x_red, start_ship_y, "pl2.png", Keyboard::A, Keyboard::D);
+  Assistant assist(start_x_assist, start_y_assist, "assistant.png",
+                   Keyboard::Space);
 
   Bullet bullet(ship.x() + ship_blue_width, ship.y());
   Bullet bullet_2(ship.x() + ship_blue_width, ship.y());
+  Bullet bullet_3(ship_2.x() + ship_red_width, ship_2.y());
+  Bullet bullet_4(ship_2.x() + ship_red_width, ship_2.y());
 
   bool menu_running = true;
+
+  Clock clock;
+  float restarting_time = 0;
+  float red_time = -1;
+  bool ability_red = false;
+  bool ability_red_use = false;
+
   //открытие окна
   sf::RenderWindow window(sf::VideoMode(800, 600), "Asteroids",
                           sf::Style::Close);
   //программа работает, пока окно открыто
   while (window.isOpen()) {
     sf::Event event;
+
+    float time = clock.getElapsedTime().asSeconds();
+    clock.restart();
+    restarting_time += time;
+
+    if (restarting_time > 10 && red_time != 5) {
+      std::cout << restarting_time << "\n";
+      ability_red = true;
+      restarting_time = 0;
+      if (ability_red) {
+        if (red_time <= 5) {
+          red_time = 5;
+        }
+      }
+      std::cout << red_time << "\n";
+    }
+
     while (window.pollEvent(event)) {
       //открываем меню по нажатию Esc
       if (event.type == sf::Event::KeyPressed &&
@@ -65,15 +95,56 @@ int main() {
     if (is_it_the_end(ship, ship_2, earthlings)) {
       ship.update();
       ship_2.update();
-      bullet.update();
-      if (bullet.destroyed) {
-        bullet.bullet_sprite.setPosition(ship.x() + ship_blue_width, ship.y());
+
+      if (event.key.code == sf::Keyboard::Space) {
+        if (ability_red) {
+          ability_red_use = true;
+          ability_red = false;
+        } else {
+          ability_red_use = false;
+        }
       }
-      if (bullet.half)
-        bullet_2.update();
-      if (bullet_2.destroyed) {
-        bullet_2.bullet_sprite.setPosition(ship.x() + ship_blue_width,
+
+      if (ability_red_use) {
+        if (red_time > 0) {
+          assist.update();
+          window.draw(assist.model_sprite);
+          red_time -= time;
+          std::cout << red_time << "\n";
+        }
+        if (red_time <= 0)
+          ability_red_use = false;
+      }
+
+      if (!ship.destroyed) {
+        bullet.update();
+        bullet.draw(window);
+        if (bullet.destroyed) {
+          bullet.bullet_sprite.setPosition(ship.x() + ship_blue_width,
                                            ship.y());
+        }
+        if (bullet.half)
+          bullet_2.update();
+        bullet_2.draw(window);
+        if (bullet_2.destroyed) {
+          bullet_2.bullet_sprite.setPosition(ship.x() + ship_blue_width,
+                                             ship.y());
+        }
+      }
+      if (!ship_2.destroyed) {
+        bullet_3.update();
+        bullet_3.draw(window);
+        if (bullet_3.destroyed) {
+          bullet_3.bullet_sprite.setPosition(ship_2.x() + ship_red_width,
+                                             ship_2.y());
+        }
+        if (bullet_3.half)
+          bullet_4.update();
+        bullet_4.draw(window);
+        if (bullet_4.destroyed) {
+          bullet_4.bullet_sprite.setPosition(ship_2.x() + ship_red_width,
+                                             ship_2.y());
+        }
       }
     }
     //вывод счета на экран
@@ -91,8 +162,6 @@ int main() {
       window.draw(ship_2.model_sprite);
     }
 
-    bullet.draw(window);
-    bullet_2.draw(window);
     window.draw(text);
 
     //отрисовка окна
